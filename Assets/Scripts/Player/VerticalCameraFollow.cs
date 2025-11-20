20bd0705ba0364eba88f7e52fd1a4dda0a2cf650
Nicolas Leon <nicolasaleon05@gmail.com>
2025-11-20 12:01:09 -0300

    Add camera movement script

---
+++ Assets/Scripts/Player/VerticalCameraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class VerticalCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float verticalOffset = 3f;
+    public float smooth = 5f;
+
+    private void LateUpdate()
+    {
+        if (!target) return;
+
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Lerp(pos.y, target.position.y + verticalOffset, Time.deltaTime * smooth);
+        transform.position = pos;
+    }
+}
